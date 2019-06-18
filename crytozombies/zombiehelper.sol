9d30557348d3ed560e6e7dfe1f8ca442a1528210
Jayce Azua <jayce.azua@gmail.com>
2019-06-17 22:59:47 -0700

    [wip] lesson 5 completed

--- crytozombies/zombiehelper.sol
+++ crytozombies/zombiehelper.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.8;
+pragma solidity ^0.4.25;
+
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
@@ -21,14 +22,14 @@ contract ZombieHelper is ZombieFeeding {
 
   function levelUp(uint _zombieId) external payable {
     require(msg.value == levelUpFee);
-    zombies[_zombieId].level++;
+    zombies[_zombieId].level = zombies[_zombieId].level.add(1);
   }
 
-  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
+  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) onlyOwnerOf(_zombieId) {
     zombies[_zombieId].name = _newName;
   }
 
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) ownerOf(_zombieId) {
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) onlyOwnerOf(_zombieId) {
     zombies[_zombieId].dna = _newDna;
   }
 
