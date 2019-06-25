35024041e80aebe3a1ede49e1bd148249ff3fd65
Jayce Azua <jayce.azua@gmail.com>
2019-06-25 00:51:07 -0700

    [wip] candidate voter app

---
+++ student_body_elections/contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.8;
+
+contract Election {
+  // store canadidate
+  // read candidate
+  string public candidate;
+  // Constructor
+  constructor() public {
+    candidate = "Candidate 1";
+
+  }
+
+}
